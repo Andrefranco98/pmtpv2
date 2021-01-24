a29784231eeb5088e26416b18bcd7efc38a722d8
Andre Franco <andrefo@ipvc.pt>
2021-01-24 12:40:13 +0000

    notif

--- app/src/main/java/com/androiddevs/firebasenotifications/Constants.kt
+++ app/src/main/java/com/androiddevs/firebasenotifications/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "ENTER SERVER KEY HERE"
+        const val SERVER_KEY = "AAAAwCtZ0_E:APA91bHL5KKnzAtzLylSgCJF0Jy75RwepIvjflFffotkJ2EB0yBUm03tePYngjoQOt2YjUnKL2_6QLKcqpjobubeSing8KpsKpu9LDbLFW7i9OdHdDHMRveoTUn817nxgoiqclorKcDl"
         const val CONTENT_TYPE = "application/json"
     }
 }
